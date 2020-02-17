098823ab22d58952bfdc886dd8092bba6c37a76f
Cristian Turcios <cristian.turcios@laureate.net>
2020-02-17 16:16:17 -0600

    save work

--- src/app/heroes/heroes.component.deep.spec.ts
+++ src/app/heroes/heroes.component.deep.spec.ts
@@ -48,4 +48,41 @@ describe('HeroesComponent (shallow tests)', () => {
       expect(heroComponentDebbugingElements[index].componentInstance.hero).toEqual(HEROES[index]);
     }
   });
+
+  it(`should call heroService.deleteHero when the Hero Component's delete button is clicken`, () => {
+    // mira si el metodo delete se llama dentro del componente
+    spyOn(fixture.componentInstance, 'delete');
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+
+    fixture.detectChanges();
+
+    const heroComponents = fixture.debugElement.queryAll(By.directive(HeroComponent));
+    // asi
+    // De esta forma podemos simular el click en algun html
+    heroComponents[0].query(By.css('button')).triggerEventHandler('click', { stopPropagation: () => {}});
+
+    // o
+    // (<HeroComponent>heroComponents[0].componentInstance).delete.emit(undefined);
+
+    // o asi
+    // heroComponents[0].triggerEventHandler('delete', null);
+    expect(fixture.componentInstance.delete).toHaveBeenCalledWith(HEROES[0]);
+  });
+
+  it('should add a new hero to the hero list when the add button is clicked', () => {
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+    fixture.detectChanges();
+    const name = 'Mr. Ice';
+    mockHeroService.addHero.and.returnValue(of({id: 5, name: name, strength: 4}));
+    // Obtenemos la referencia al input
+    const inputElement =  fixture.debugElement.query(By.css('input')).nativeElement;
+    const addButton = fixture.debugElement.queryAll(By.css('button'))[0];
+    // asignandole simulamos que el usuario introdujo el texto
+    inputElement.value = name;
+    addButton.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    const heroText = fixture.debugElement.query(By.css('ul')).nativeElement.textContent;
+    expect(heroText).toContain(name);
+  });
 });
